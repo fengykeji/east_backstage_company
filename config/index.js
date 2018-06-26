4c747fe9ab0d7e792d7f8d1b060f9f584627adfa
canny-xi <331230985@qq.com>
2018-06-26 18:13:12 +0800

    x

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/admin/conpany/admin_conpany_show/',
+    assetsPublicPath: '/admin/company',
 
     /**
      * Source Maps
