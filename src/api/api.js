e204c5d7c92a46b0ec1701ba4d3a085fe4e2a68a
canny-xi <331230985@qq.com>
2018-07-18 10:25:57 +0800

    x

--- src/api/api.js
+++ src/api/api.js
@@ -4,11 +4,11 @@ import Vue from 'vue'
 //请求带上cookie
 // http.defaults.withCredentials = true;
 
-let base = 'http://120.78.69.178:2902/';
-Vue.prototype.base = 'http://120.78.69.178:2902/';    //正式服地址
+// let base = 'http://120.78.69.178:2902/';
+// Vue.prototype.base = 'http://120.78.69.178:2902/';    //正式服地址
 
-// let base = 'http://120.27.21.136:2798/';
-// Vue.prototype.base = 'http://120.27.21.136:2798/';  //测试服地址
+let base = 'http://120.27.21.136:2798/';
+Vue.prototype.base = 'http://120.27.21.136:2798/';  //测试服地址
 
 const doPost = function (url) {
   return function (params) {
